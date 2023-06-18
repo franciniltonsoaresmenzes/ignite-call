dde43b18c6134bcc3c037815d72f8d963bc81713
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-06-17 22:59:48 -0300

    Manipulando cookies no Next

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,37 @@
+import { prisma } from '@/lib/prisma'
+import { setCookie } from 'nookies'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const userExist = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExist)
+    return res.status(400).json({ message: 'Username already taken.' })
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  setCookie({ res }, '@ignite-call:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 dias
+    path: '/',
+  })
+
+  return res.status(201).json(user)
+}
